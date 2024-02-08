0f47fb86f59fa652c380bf382b8f5146c53df324
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-02-08 17:10:25 +0000

    Gallery class finished and test

--- src/main/java/Gallery.java
+++ src/main/java/Gallery.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Gallery {
+import java.util.ArrayList;
+
+public class Gallery {
+
+    private String name;
+    private float till;
+    private ArrayList<Artwork> artworks;
+
+    public Gallery (String galleryName, float galleryTill) {
+        this.name = galleryName;
+        this.till = galleryTill;
+        this.artworks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getTill() {
+        return till;
+    }
+
+    public void setTill(float till) {
+        this.till = till;
+    }
+
+    public ArrayList<Artwork> getArtworks() {
+        return artworks;
+    }
+
+    public void setArtworks(ArrayList<Artwork> artworks) {
+        this.artworks = artworks;
+    }
+
+    public void addArtwork (Artwork artwork) {
+        this.artworks.add(artwork);
+    }
+
+    public void removeArtwork (Artwork artwork) {
+        this.artworks.remove(artwork);
+    }
+
+    public  int stock(){
+        return this.artworks.size();
+    }
+
+
 }
--- src/test/java/GalleryTest.java
+++ src/test/java/GalleryTest.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class GalleryTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class GalleryTest {
+
+
+    Gallery gallery;
+    Artwork monaLisa;
+    Artwork salvatorMundi;
+    Artwork theLastSupper;
+    Artist leonardo;
+
+    @BeforeEach
+    public void setUp() {
+        gallery = new Gallery("Anchiano Gallery", 20000);
+        monaLisa = new Artwork("Mona Lisa", "Da Vinci", 1000);
+        salvatorMundi = new Artwork("Salvator Mundi", "Da Vinci", 2000);
+        theLastSupper = new Artwork("The Last Supper", "Da Vinci",3000);
+        leonardo = new Artist("Leonardo");
+        gallery.addArtwork(monaLisa);
+        gallery.addArtwork(salvatorMundi);
+        gallery.addArtwork(theLastSupper);
+    }
+
+    @Test
+    public void hasName(){
+        assertThat(gallery.getName()).isEqualTo("Anchiano Gallery");
+    }
+
+    @Test
+    public void canSetName(){
+        gallery.setName("London Gallery");
+        assertThat(gallery.getName()).isEqualTo("London Gallery");
+    }
+
+
+    @Test
+    public void canGetTill(){
+        assertThat(gallery.getTill()).isEqualTo(20000);
+    }
+
+    @Test
+    public void canSetTill(){
+        gallery.setTill(15000);
+        assertThat(gallery.getTill()).isEqualTo(15000);
+    }
+
+    @Test
+    public void canGetArtworks(){
+        assertThat(gallery.stock()).isEqualTo(3);
+    }
+
+    @Test
+    public void hasStock(){
+//        gallery.removeArtwork(monaLisa);
+//        gallery.removeArtwork(salvatorMundi);
+//        gallery.removeArtwork(theLastSupper);
+
+//        When removes are added, we expect the test to fail
+        assertThat(gallery.stock()).isGreaterThanOrEqualTo(1);
+    }
+
+
+
 }
