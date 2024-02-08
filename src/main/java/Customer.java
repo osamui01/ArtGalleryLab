e1d0ca06e8b2d43014c06a634eb5f2a6727563cc
Ralphael Obadiaru <ralph.obadiaru@gmail.com>
2024-02-08 17:09:57 +0000

    Customer class created and tested

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Customer {
+public class Customer {
+
+    private String name;
+    private float money;
+
+    public Customer(String customerName, float customerMoney){
+        this.name = customerName;
+        this.money = customerMoney;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String customerName) {
+        this.name = customerName;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    public void removeMoney(float price){
+        this.money -= price;
+    }
+
+
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class CustomerTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CustomerTest {
+
+
+    Customer customer;
+
+    @BeforeEach
+    public void setUp(){
+
+      customer = new Customer("Leonardo", 7000);
+    }
+
+   @Test
+   public void canGetName(){
+        assertThat(customer.getName()).isEqualTo("Leonardo");
+   }
+
+    @Test
+    public void canSetName(){
+        customer.setName("Van Gogh");
+        assertThat(customer.getName()).isEqualTo("Van Gogh");
+    }
+
+    @Test
+    public void canGetMoney(){
+        assertThat(customer.getMoney()).isEqualTo(100);
+    }
+
+    @Test
+    public void canSetMoney(){
+        customer.setMoney(250);
+        assertThat(customer.getMoney()).isEqualTo(250);
+    }
+
+    @Test
+    public void canRemoveMoney(){
+        customer.setMoney(250);
+        customer.removeMoney(20);
+        assertThat(customer.getMoney()).isEqualTo(230);
+    }
+
+
 }
